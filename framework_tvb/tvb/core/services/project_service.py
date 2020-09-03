73f7f5a9abaec676aa966edfa003044f3b269b7d
bvalean <bogdan.valean@codemart.ro>
2020-09-03 13:26:30 +0300

    HBP-113 Fix failing test

--- framework_tvb/tvb/core/services/project_service.py
+++ framework_tvb/tvb/core/services/project_service.py
@@ -321,9 +321,8 @@ class ProjectService:
             for one_link in links:
                 dao.remove_entity(Links, one_link.id)
 
-            self.structure_helper.remove_project_structure(project2delete.name)
-
             project_folder = self.structure_helper.get_project_folder(project2delete)
+            self.structure_helper.remove_project_structure(project2delete.name)
             encrypted_path = encryption_handler.compute_encrypted_folder_path(project_folder)
             if os.path.exists(encrypted_path):
                 if os.path.isdir(encrypted_path):
